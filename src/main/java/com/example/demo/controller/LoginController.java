35cbae599a65811e32ef1f4e61847e56f609c2f2
Cloudy1112 <chonglevan2004@gmai.com>
2025-11-25 13:43:41 +0700

    refactor: RequestParam /dang-nhap (LoginController)

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -16,9 +17,11 @@ public class LoginController {
 	}
 	
 	@PostMapping("/dang-nhap")
-	public String login(HttpServletRequest req, ModelMap model) {
-		String username = req.getParameter("username").toString();
-		String password = req.getParameter("password").toString();
+	public String login(HttpServletRequest req, ModelMap model, 
+						@RequestParam ("username") String username, //Dùng RequestParam để lấy tham số username & password và ép kiểu dữ liệu
+						@RequestParam ("password") String password) {
+		//String username = req.getParameter("username").toString();
+		//String password = req.getParameter("password").toString();
 		
 		if (username.equals("trung") && password.equals("123")) {
 			model.addAttribute("uuid", username);
