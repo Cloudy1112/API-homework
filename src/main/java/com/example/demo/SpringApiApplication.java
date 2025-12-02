ce72bd506171657ee693309e532a88813f0467f5
Cloudy1112 <chonglevan2004@gmai.com>
2025-12-02 17:21:12 +0700

    fix: error POM (add @SpringBootApplication in SpringApiApplication)

--- src/main/java/com/example/demo/SpringApiApplication.java
+++ src/main/java/com/example/demo/SpringApiApplication.java
@@ -2,9 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-
+@SpringBootApplication
 public class SpringApiApplication {
 
 	public static void main(String[] args) {
